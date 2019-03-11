d319c36838170610a79ae91fb415b5b1d9e26906
B-ry C <doybrian@B-rys-MBP.hsd1.ca.comcast.net>
2019-03-11 16:12:41 -0700

    all files for a fully functional RPS multiplayer game

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RPS--Multiplayer
 A multiplayer app for Rock Paper Scissors game
+
+### Overview
+
+This is a Rock Paper Scissors game that is an online multiplayer game, all with the help of Firebase and the necessary web development elements.
+
+### Game Design Setup
+
+* The game will match two hand choices at a time. Flags are set up so that the game can keep track of when the player picks a choice and if another player has made one to pair with for the round/match. Once a round or match is decided, the database will be cleared to make way for another round of hand choices. Basically, the first two hand choices that gets fired up will be paired together. This serves the multiplayer functionality of the game. 
+
+* Both players pick either `rock`, `paper` or `scissors` by clicking on the corresponding images. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+
+* The game will track each player's wins, losses and ties.
+
+* There is a chat functionality in this game. No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+
+* Styling and theme is simple. There is a message board added so all instant messages will be posted, who has handed a hand choice and who the winner/loser is or if it is a tie. Because of the simplicity of the styling, sound effects have been added and an intro music.
+
+*The scope of the project is that it assumes the game will be played in a "perfect" scenario in which everybody just takes turns and picks a hand one after the another . As a future development, will look into situations when players choose a hand at the same time, or when players have the same names.
---
+++ assets/javascript/game.js
@@ -0,0 +1,188 @@
+/* global moment firebase */
+    // Initialize Firebase
+    // Make sure to match the configuration to the script version number in the HTML
+    // (Ex. 3.0 != 3.7.0)
+    var config = {
+        apiKey: "AIzaSyBX8kYiMF6cLoPj7JCtTkTO8w9riKNgZiU",
+        authDomain: "rock-paper-scissors-4bca5.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissors-4bca5.firebaseio.com",
+        projectId: "rock-paper-scissors-4bca5",
+        storageBucket: "rock-paper-scissors-4bca5.appspot.com",
+        messagingSenderId: "724862951295"
+    };
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+
+    //variable declarations and initialization
+    var player_name, player_hand;
+    var opponent, opponentHand;
+    var wins = 0;
+    var losses = 0;
+    var ties = 0;
+    var flag = false; //flag if player has entered choice
+    var flag2 = false; //flag if opponent has entered choice
+    var instant_message;
+    var sound_effect;
+
+
+    // At the page hand value updates, get a snapshot of the local data.
+    // This function allows you to update your page in real-time when the values within the firebase node handata changes
+    database.ref("/handData").on("value", function(snapshot) {
+
+        // Set the local variables for opponent equal to the stored values in firebase.
+       if (snapshot.child("opponent").exists() && snapshot.child("opponentHand").exists()) {
+
+           if (snapshot.val().opponent != null || snapshot.val().opponentHand != null) {
+
+                if (player_name !== snapshot.val().opponent) {
+                    
+                    opponent = snapshot.val().opponent;
+                    opponentHand = snapshot.val().opponentHand;
+                    console.log(opponent);
+                    console.log(opponentHand);
+
+                    $("#message_board").prepend('<p>' + opponent + ' has picked a hand!</p>');
+
+                    sound_effect = new sound("./assets/audio/Alert.mp3");
+                    sound_effect.play();
+                    
+                    flag2 = true;
+                }
+                check_winner();                  
+            } 
+        }
+        // If any errors are experienced, log them to console.
+        }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+
+    //event listener when a new message is sent to the database
+    database.ref("/messaging").on("value", function(snap) {
+
+        // Set the local variables for opponent equal to the stored values in firebase.
+       if (snap.child("user_name").exists() && snap.child("IMessage").exists()) 
+
+         $("#message_board").prepend('<p>' + snap.val().user_name + ' says: ' + snap.val().IMessage + '</p>');
+
+        sound_effect = new sound("./assets/audio/instant.mp3");
+        sound_effect.play();
+
+        //clears all information in database after each round
+        database.ref("/messaging").set({
+            user_name: null,
+            IMessage: null
+        });
+        
+        }, function(errorObject) {
+
+        // In case of error this will print the error
+        console.log("The read failed: " + errorObject.code);
+    });
+
+
+    //checks winner after player and opponent has entered choice
+    function check_winner() {
+        if (flag && flag2) {
+
+            if ((player_hand === "r" && opponentHand === "s") ||
+                (player_hand === "s" && opponentHand === "p") || 
+                (player_hand === "p" && opponentHand === "r")) {
+                wins++;
+                $("#message_board").prepend('<p>' + player_name + ' vs. ' + opponent + '. You won! Wins: ' + wins + ' Losses: ' + losses + ' Ties: ' + ties + ' </p>');
+            } else if (player_hand === opponentHand) {
+                ties++;
+                $("#message_board").prepend('<p>' + player_name + ' vs. ' + opponent + '. It is a tie!  Wins: ' + wins + ' Losses: ' + losses + ' Ties: ' + ties + ' </p>');
+            } else {
+                losses++;
+                $("#message_board").prepend('<p>' + player_name + ' vs. ' + opponent + '. You lost! Wins: ' + wins + ' Losses: ' + losses + ' Ties: ' + ties + ' </p>');
+            }
+            
+            sound_effect = new sound("./assets/audio/Boxing_Bell.mp3");
+            sound_effect.play();
+
+            //reset flags after each round is done
+            flag = false;
+            flag2 = false;
+
+            //clears all information in database after each round
+            database.ref("/handData").set({
+            opponent: null,
+            opponentHand: null
+        });
+        }
+    }
+
+    // Whenever a user clicks the start button
+    $("#start_button").on("click", function(event) {
+        event.preventDefault(); 
+
+            player_name = $("#name-input").val().trim();
+            player_name = player_name.toUpperCase();
+
+            $("iframe").remove();
+            $("#start_button").hide();
+            $("#entry-form").hide();
+            $("#name-input").val('');
+            $("#name-input").hide();
+            $("#online_users").text("Click on a hand to play a round."); 
+            $("#message_board").empty();
+            $("#scorecard").show();
+            $("#message-form").show();
+            $("#status").show(); 
+            //$("body").prepend('<audio autoplay loop  id="playAudio"><source src="bensound-theduel.mp3"></audio>');
+           
+    }); 
+
+    //function when a hand is picked to play game
+    $(".clicked").on("click", function() {
+
+        player_hand = $(this).attr("data-btnID");
+
+        $("#message_board").prepend('<p>You have picked a hand.</p>');
+
+        sound_effect = new sound("./assets/audio/Drop.mp3");
+        sound_effect.play();
+
+        database.ref("/handData").set({
+            opponent: player_name,
+            opponentHand: player_hand
+        });
+
+        flag = true;
+
+        check_winner();
+
+    });
+
+    //funtion for grabbing instant message and sendind it to firebase
+    $("#IM_button").on("click", function() {
+
+        instant_message = $("#message-area").val().trim();
+        $("#message-area").val('');
+
+
+        database.ref("/messaging").set({
+            user_name: player_name,
+            IMessage: instant_message
+        });
+
+    });
+
+    //sound function
+    function sound(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function(){
+        this.sound.play();
+        }
+        this.stop = function(){
+        this.sound.pause();
+        }
+    }
